82c30b3d8326318bc464b548b68deeca5ab865b8
Alexandre Deniau <deniaualexandre@gmail.com>
2020-05-13 15:52:00 +0200

    e

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  # resources :restaurants
 
+  # resources :restaurants do
+  #   resources :reviews
+  # end
   resources :restaurants do
-    resources :reviews
+    resources :reviews, only: [ :new, :create ]
   end
- 
+  resources :reviews, only: [ :destroy ]
 end
