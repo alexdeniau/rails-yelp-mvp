380a88439d1c7014d587a451b89a23477afc0876
Alexandre Deniau <deniaualexandre@gmail.com>
2020-05-13 14:23:14 +0200

    e

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, :category, :content, :rating, :phone_number, presence: true
-  validates :category, inclusion: { :in => ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :name, :address, :category, :phone_number, presence: true
+  validates :category, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :phone_number, format: {with: /\A(0|\+\d{2}\s*|00\s*\d{2}\s*)(\d\s*){9}\z/}
 end
